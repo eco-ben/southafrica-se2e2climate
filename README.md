7c03fe8509eacd54ec1cc1d2e56cb04b867790c8
Benjamin Grier <benjamin.grier@strath.ac.uk>
2025-12-18 16:15:03 +0000

    commit changes before moving computers

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
 # southafrica-se2e2climate
-Analysis code for StrathE2E2 climate change paper using the Southern Benguela - South Africa MA implementation. Code will also include sensitivity analysis of model inputs.
+Analysis code for StrathE2E2 climate change paper using the Southern Benguela - South Africa MA implementation.
+
+# Setup
+
+Full recreation of this project requires that users have Julia v1.11.6 and Quarto installed.
+Set up the project in the usual Julian way by instantiating the project.
+
+Start Julia from the project root (the main project directory, where the README file sits).
+
+```shell
+julia --project=.
+```
+
+Followed by:
+
+```julia
+] instantiate
+```
+
+The above will setup the project and install all dependencies.
+
+Note that the specific version of Rasters.jl and Makie.jl currently has an error preventing
+precompilation of the Makie extension for Rasters.jl (RastersMakieExt.jl). An error message
+will appear, but can be safely ignored as we do not rely on this extension for any
+visualizations.
+
+All scripts are expected to be run from the `src` directory.
+The current directory can be changed without leaving Julia by hitting semi-colon.
+
+```julia
+; cd src
+```
+
+## Structure
+
+Repository structure:
+``` code
+ADRIA-reef-indicators/
+├─ src/     # Analysis scripts
+├─ outputs/     # ADRIA results sets and analysis / figure outputs
+├─ data/
+├─ paper/   # Reproducible quarto manuscript via the rendering of .qmd file
+├─ .gitignore
+├─ config.toml  # configuration file for project
+├─ Project.toml     # Julia project spec
+├─ Manifest.toml    # Julia project spec
+└─ README.md    # this file
+```
+
+The ADRIA Domain data package `GBR_2024_10_15_HighResCoralStress` is required to run ADRIA
+for this project.
+
+The path to this domain folder should be specified in the config.toml file.
+
+The following structure and files should be maintained in the outputs, figs and data folders:
+``` code
+ADRIA-reef-indicators
+│───outputs
+│   └───ADRIA_results
+│       └───HighResCoralStress
+│           ├───processed_model_outputs     # Folder containing scenario-median timeseries arrays for each GCM
+│           │       ├───median_cover_ACCESS-CM2.nc
+│           │       ├───median_cover_ACCESS-ESM1-5.nc
+│           │       ├───median_cover_EC-Earth3-Veg.nc
+│           │       ├───median_cover_GFDL-CM4.nc
+│           │       └───median_cover_NorESM2-MM.nc
+│           ├───HighResCoralStress_ADRIA_scens_ACCESS-CM2.csv       # CSV files containing the ADRIA-CoralBlox input parameter scenarios used in 1_.jl for each GCM
+│           ├───HighResCoralStress_ADRIA_scens_ACCESS-ESM1-5.csv
+│           ├───HighResCoralStress_ADRIA_scens_EC-Earth3-Veg.csv
+│           ├───HighResCoralStress_ADRIA_scens_GFDL-CM4.csv
+│           ├───HighResCoralStress_ADRIA_scens_NorESM2-MM.csv
+│           ├───clustered_reefs_carbonate.gpkg      # Reef spatial data with cluster timeseries information attached (before context layers are attached in 3_.jl).
+│           └───analysis_context_layers_carbonate.gpkg      # Geopackage containing the required timeseries clustering, connectivity and carbonate budget values for each reef
+│───figs    # Directory directly containing general GCM-wide and paper-methods figures, as well as GCM subdirectories
+│   ├───ACCESS-CM2
+│   │   ├───bioregion
+│   │   ├───gbr
+│   │   └───management_area
+│   ├───ACCESS-ESM1-5
+│   │   ├───bioregion
+│   │   ├───gbr
+│   │   └───management_area
+│   ├───EC-Earth3-Veg
+│   │   ├───bioregion
+│   │   ├───gbr
+│   │   └───management_area
+│   ├───GFDL-CM4
+│   │   ├───bioregion
+│   │   ├───gbr
+│   │   └───management_area
+│   └───NorESM2-MM
+│       ├───bioregion
+│       ├───gbr
+│       └───management_area
+└───data
+    ├─ GBRMPA_Management_Areas.gpkg    # File containing polygons of GBRMPA management areas
+    ├─ GBRMPA_Reef_Features.gpkg    # File containing polygons of GBRMPA reef feature data
+    └─ GBRMPA_Reefal_Bioregions.gpkg   # File containing polygons of GBRMPA reefal bioregions.
+```
+Additionally, if available, ADRIA Result Sets should be located in the outputs/ADRIA_Results/HighResCoralStress/ folder.
--- src/1_initial_climate_change/1_initial_climate_change_montecarlo.R
+++ src/1_initial_climate_change/1_initial_climate_change_montecarlo.R
@@ -2,7 +2,8 @@ library(StrathE2E2)
 library(tidyverse)
 library(furrr)
 
-plan(multisession, workers = 4)
+n_workers <- 20
+plan(multisession, workers = n_workers)
 
 model_path <- "../../StrathE2E_workspace/Models/"
 variants <- list.dirs(str_glue("{model_path}South_Africa_MA/"), recursive = FALSE)
@@ -59,21 +60,6 @@ load_and_run_variants <- function(
     return()
 }
 
-load_and_run_montecarlo <- function(model_variant, model_name, model_path, nyears = 50, n_iter = 1000, result_path = ".") {
-    print(str_glue("Running monte carlo for South Africa MA {model_variant}"))
-
-    model <- e2e_read(
-        model.name = model_name,
-        model.variant = model_variant,
-        models.path = model_path,
-        results.path = result_path
-    )
-    results <- e2e_run_mc(model, nyears = nyears, n_iter = n_iter, csv.output = TRUE)
-
-
-    return()
-}
-
 future_map(
     variants,
     function(x) {
@@ -94,10 +80,134 @@ future_map(
     .progress = TRUE
 )
 
-future_map(
-    variants,
-    function(x) {
-        load_and_run_montecarlo(x, "South_Africa_MA", model_path, result_path = paste0(results_path_base, x))
-    },
-    .progress = TRUE
-)
+baseline_variants <- c("CNRM" = "2010-2019-CNRM-ssp126", "GFDL" = "2010-2019-GFDL-ssp126")
+n_iter = 1000
+base_result_directory <- "./outputs/initial_runs/monte_carlo_results/"
+
+for (variant in c(variants[variants %in% baseline_variants], variants[!variants %in% baseline_variants])) {
+    esm <- str_extract(variant, "[:upper:]{4}")
+    base_parms_ident <- paste0(baseline_variants[esm], "-MC")
+
+    baseline_mode <- FALSE
+    if (variant %in% baseline_variants) {
+        baseline_mode <- TRUE
+    }
+
+    variant_result_dir <- file.path(base_result_directory, "South_Africa_MA", variant, "CredInt")
+    
+    if (!dir.exists(variant_result_dir)) {
+        dir.create(variant_result_dir, recursive=TRUE)
+    }
+    
+    if (!baseline_mode) {
+      baseline_param_files <- list.files(file.path(
+        base_result_directory, 
+        "South_Africa_MA", 
+        baseline_variants[esm], 
+        "CredInt"
+      ), pattern = "parameters", full.names = TRUE)
+      file.copy(baseline_param_files, variant_result_dir)
+    }
+    
+
+    model_setup <- data.frame(
+        region_name = "South_Africa_MA",
+        model_variant = variant,
+        model_path = model_path,
+        result_path = base_result_directory
+    )
+    batch_setup <- do.call("rbind", replicate(n_workers, model_setup, simplify = FALSE))
+    batch_setup$batch_id <- seq_len(n_workers)
+
+    if (baseline_mode) {
+        model_ident_label <- "baseline"
+    } else {
+        model_ident_label <- "scenario"
+    }
+    batch_setup$batch_model_ident <- paste(batch_setup$model_variant, "batch", batch_setup$batch_id, sep = "-")
+
+    batch_setup$n_iter <- n_iter / n_workers
+    batch_setup$begin_sample <- NA
+
+    if (baseline_mode) {
+        batch_setup$n_iter <- ifelse(batch_setup$batch_id == 1, batch_setup$n_iter, batch_setup$n_iter + 1)
+    } else {
+        for (r in seq_len(n_workers)) {
+            if (r == 1) {
+                batch_setup[r, ]$begin_sample <- 1
+            } else {
+                batch_setup[r, ]$begin_sample <- (batch_setup[r, ]$n_iter * batch_setup[r - 1, ]$batch_id + 1)
+            }
+        }
+    }
+
+    batch_setup$baseline_mode <- baseline_mode
+    batch_setup$base_parms_ident <- base_parms_ident
+
+    batch_list <- split(batch_setup, batch_setup$batch_id)
+    
+    print(paste0("Running Monte Carlo simulations for ", variant))
+    
+    future_map(
+        batch_list,
+        function(x) {
+          baseline_mode <- x$baseline_mode
+            model <- e2e_read(
+                x$region_name,
+                x$model_variant,
+                models.path = x$model_path,
+                results.path = x$result_path,
+                model.ident = x$batch_model_ident
+            )
+
+            if (baseline_mode) {
+                e2e_run_mc(
+                    model,
+                    baseline.mode = baseline_mode,
+                    n_iter = x$n_iter,
+                    csv.output = TRUE,
+                    runtime.plot = FALSE,
+                    postprocess = FALSE
+                )
+            } else {
+                e2e_run_mc(
+                    model,
+                    baseline.mode = baseline_mode,
+                    baseparms.ident = x$base_parms_ident,
+                    begin.sample = x$begin_sample,
+                    n_iter = x$n_iter,
+                    csv.output = TRUE,
+                    runtime.plot = FALSE,
+                    postprocess = FALSE
+                )
+            }
+
+            return()
+        },
+        .progress = TRUE,
+        .options = furrr_options(seed = TRUE)
+    )
+
+    combined_model <- e2e_read(
+        "South_Africa_MA",
+        variant,
+        models.path = model_path,
+        results.path = base_result_directory,
+        model.ident = paste0(variant, "-MC")
+    )
+    combined_data <- e2e_merge_sens_mc(
+        combined_model,
+        selection = "MC",
+        ident.list = batch_setup$batch_model_ident,
+        postprocess = FALSE,
+        csv.output = TRUE
+    )
+    processed_data <- e2e_process_sens_mc(
+        combined_model,
+        selection = "MC",
+        csv.output = TRUE
+    )
+    
+    batch_files <- list.files(variant_result_dir, full.names = TRUE, pattern = "batch")
+    file.remove(batch_files)
+}
